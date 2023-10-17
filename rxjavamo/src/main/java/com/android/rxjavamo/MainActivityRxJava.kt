b26180362b361959f0d2a8a76d48db364b3fd2ec
Kmlesh Patel <pkamlesh0124@gmail.com>
2023-10-17 08:57:55 +0530

    Worked on reactive programming and created module for it

---
+++ rxjavamo/src/main/java/com/android/rxjavamo/MainActivityRxJava.kt
@@ -0,0 +1,11 @@
+package com.android.rxjavamo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRxJava : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_rx_java)
+    }
+}
